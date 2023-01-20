43723e6c31792b2591251ec13b1747df7af3bb6d
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2023-01-19 21:56:35 -0300

    Image background created

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
 
+import BackgroundImageWithChildren from '../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren'
+
 const StyledDiv = styled.div`
   background-color: red;
 `
 
 export default function HomePage() {
   return (
-    <StyledDiv>
+    <BackgroundImageWithChildren>
+      <StyledDiv>
       HomePage
-    </StyledDiv>
+      </StyledDiv>
+    </BackgroundImageWithChildren>
+    
   )
 }
